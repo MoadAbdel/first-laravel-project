ce472965a6e1101f7e642feec95b224fa0f1b6f4
moad abdel <waheb.moad@gmail.com>
2026-03-09 16:24:59 +0100

    activité 7

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/', [IndexController::class, 'index']);
+Route::get('/', [IndexController::class, 'index'])->name('home');
 
-Route::get('/articles/{id}', [ArticleController::class, 'show']);
+Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('article.details');
 
