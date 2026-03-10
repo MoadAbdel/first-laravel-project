937fab5de1d640b54298ba983047d290d35c37b4
moad abdel <waheb.moad@gmail.com>
2026-03-10 17:08:09 +0100

    activité 16

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 
 <body>
