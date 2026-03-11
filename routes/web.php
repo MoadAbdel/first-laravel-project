db2d185f7b58b40fb11b25d641de91c384f48197
AbdelM <116069298+MoadAbdel@users.noreply.github.com>
2026-03-11 14:20:18 +0100

    activité 17-4 fix

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\ArticleController;
 Route::get('/', [IndexController::class, 'index'])->name('home');
 
 Route::prefix('articles')->group(function () {
-    Route::get('/creer', [ArticleController::class, 'create']);->name('creer');
-    Route::get('/modifier/{id}', [ArticleController::class, 'update']);->name('modifier');
-    Route::get('/supprimer/{id}', [ArticleController::class, 'delete']);->name('supprimer');
+    Route::get('/creer', [ArticleController::class, 'create'])->name('creer');
+    Route::get('/modifier/{id}', [ArticleController::class, 'update'])->name('modifier');
+    Route::get('/supprimer/{id}', [ArticleController::class, 'delete'])->name('supprimer');
     Route::get('/{id}', [ArticleController::class, 'show'])->name('article.details');
 });
 
